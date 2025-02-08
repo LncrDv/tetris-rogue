997c35e825e75c09d5a3222a8c53e79ef02f841b
LncrDv <lncrdv.pro@gmail.com>
2025-02-08 17:13:53 +0100

    TetrisBlock: bool ValidMove()
    Created the bool function ValidMove to check for a valid move for the piece, as well as adding new width and height for the background.

--- TetrisRogue/Assets/Scripts/TetrisBlock.cs
+++ TetrisRogue/Assets/Scripts/TetrisBlock.cs
@@ -6,6 +6,8 @@ public class TetrisBlock : MonoBehaviour
 {
     private float previousTime;
     public float fallTime;
+    public static int height = 20;
+    public static int width = 10;
 
     // Update is called once per frame
     void Update()
@@ -13,16 +15,38 @@ public class TetrisBlock : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left;
+            if(!ValidMove())
+                transform.position += Vector3.right;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += Vector3.right;
+            if (!ValidMove())
+                transform.position += Vector3.left;
         }
 
         if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime/10 : fallTime))
         {
             transform.position += Vector3.down;
+            if (!ValidMove())
+                transform.position += Vector3.up;
             previousTime = Time.time;
         }
+    }
+
+    bool ValidMove()
+    {
+        foreach(Transform child in transform)
+        {
+            int roundedX = Mathf.RoundToInt(child.position.x);
+            int roundedY = Mathf.RoundToInt(child.position.y);
+
+            if(roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
